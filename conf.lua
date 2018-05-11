18281dd2029c9133f8838438547bb42d7ec0ed24
maritaria <bramkamies@gmail.com>
2018-05-11 16:09:35 +0200

    Corrected app identity

--- conf.lua
+++ conf.lua
@@ -5,7 +5,7 @@ C.screenHeight = 60
 C.canvasHeight = C.screenHeight - 10
 
 function love.conf(t)
-    t.identity = "TinyScreen"
+    t.identity = "PixelEditor"
     t.version = "11.0"
     t.console = false
 
